a6d3058af487b8643205b00ca8174a8bf895f216
Paris Ward <wardparis.pw@gmail.com>
2018-04-02 23:27:45 -0700

    added zipcode search form, and forEach loop to iterate through required data to add markers and prepend a list

--- src/index.js
+++ src/index.js
@@ -1,54 +1,52 @@
-var lat,lng;
+
 var url = "https://api.openchargemap.io/v2/poi/?output=json&countrycode=US&distance=&distanceunit=miles&maxresults=5&opendata=true&compact=true&verbose=false&includecomments=true&latitude=";
 
-//Obtain users current location in latitude and longitude
-$(document).ready(function(){
-    if(navigator.geolocation) {      navigator.geolocation.getCurrentPosition(function(position){
-            lat = position.coords.latitude;
-            console.log(lat);
-            lng = position.coords.longitude;
-            console.log(lng);
-            initMap(lat,lng);
-            findDock(lat,lng);
-        })     
-    }  
-})
-//Initialize map with current location marker
-function initMap(lat,lng) { 
+function generateMap() { 
+ if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var api = url + lat + "&" + "longitude=" + lng;
+    console.log(api);
     var userPosition = {lat: lat, lng: lng};
-    
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
+        zoom: 12,
         center: userPosition
-    });
-    var userMarker = new google.maps.Marker({
-       position: userPosition,
-       map: map
-   });
-
-}
-//Find closest charging dock
-function findDock(lat,lng) {
-  var api = url + lat + "&" + "longitude=" + lng;
-  console.log(api);
-  $.ajax ({
-    url: api,
-    type: 'GET',
-    dataType:'JSON',
-    success: data => {
-      //Results go from closest(0) to furthest(4) away from user's current position
-       var addressOne = data[0]['AddressInfo'];
-       var addressTwo = data[1]['AddressInfo'];
-       var addressThree = data[2]['AddressInfo'];
-       var addressFour = data[3]['AddressInfo'];
-       var addressFive = data[4]['AddressInfo'];
+     });
       
-        
-      console.log(addressOne.Latitude);
-      console.log(addressOne.Longitude);
-      //console.log(addressTwo);
-   
-    
-    }
-  })  
-}
+     $.ajax ({
+     url: api,
+     type: 'GET',
+     dataType:'JSON',
+     success: data => {  
+          const user = new google.maps.Marker({
+          position: userPosition,
+          map: map,
+          animation: google.maps.Animation.DROP
+          });
+
+           data.forEach(location => {
+           var address = {};
+
+           address.lat = location.AddressInfo.Latitude;
+           address.lng = location.AddressInfo.Longitude;
+           address.address = location.AddressInfo.AddressLine1;
+           address.comments = location.AddressInfo.AccessComments;
+           address.distance = location.AddressInfo.Distance;
+           address.zipCode = location.AddressInfo.Postcode;
+           console.log(address.address);
+
+           const docks = new google.maps.Marker({
+           position: {lat:address.lat, lng:address.lng},
+           map: map
+          
+       });
+     });
+   },
+     error:error => {
+       console.log(error);
+       }
+     });     
+    });
+   }     
+ };
